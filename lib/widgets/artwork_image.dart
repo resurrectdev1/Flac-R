9b44f36166215a73aeb1dfcf7a616abbe586378f
Resurrect <resurrectdev@protonmail.com>
2026-07-18 21:18:58 -0400

    Format code for better readability in artwork_image.dart

--- lib/widgets/artwork_image.dart
+++ lib/widgets/artwork_image.dart
@@ -13,12 +13,12 @@ class ArtworkImage extends StatefulWidget {
     this.placeholderChild,
   });
 
-  final String     path;
-  final bool       hasArtwork;
-  final double     size;
-  final double     borderRadius;
-  final Color      placeholderColor;
-  final Widget?    placeholderChild;
+  final String path;
+  final bool hasArtwork;
+  final double size;
+  final double borderRadius;
+  final Color placeholderColor;
+  final Widget? placeholderChild;
 
   @override
   State<ArtworkImage> createState() => _ArtworkImageState();
@@ -26,7 +26,7 @@ class ArtworkImage extends StatefulWidget {
 
 class _ArtworkImageState extends State<ArtworkImage> {
   Uint8List? _bytes;
-  bool       _loaded = false;
+  bool _loaded = false;
 
   @override
   void initState() {
@@ -38,14 +38,21 @@ class _ArtworkImageState extends State<ArtworkImage> {
   void didUpdateWidget(ArtworkImage old) {
     super.didUpdateWidget(old);
     if (old.path != widget.path) {
-      setState(() { _bytes = null; _loaded = false; });
+      setState(() {
+        _bytes = null;
+        _loaded = false;
+      });
       if (widget.hasArtwork) _load();
     }
   }
 
   Future<void> _load() async {
     final bytes = await ArtworkCache.instance.get(widget.path);
-    if (mounted) setState(() { _bytes = bytes; _loaded = true; });
+    if (mounted)
+      setState(() {
+        _bytes = bytes;
+        _loaded = true;
+      });
   }
 
   @override
@@ -53,13 +60,19 @@ class _ArtworkImageState extends State<ArtworkImage> {
     return ClipRRect(
       borderRadius: BorderRadius.circular(widget.borderRadius),
       child: SizedBox(
-        width: widget.size, height: widget.size,
+        width: widget.size,
+        height: widget.size,
         child: _loaded && _bytes != null
-        ? Image.memory(_bytes!, width: widget.size, height: widget.size, fit: BoxFit.cover)
-        : Container(
-          color: widget.placeholderColor,
-          child: widget.placeholderChild,
-        ),
+            ? Image.memory(
+                _bytes!,
+                width: widget.size,
+                height: widget.size,
+                fit: BoxFit.cover,
+              )
+            : Container(
+                color: widget.placeholderColor,
+                child: widget.placeholderChild,
+              ),
       ),
     );
   }
